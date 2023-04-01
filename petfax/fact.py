1b57511baa7ede3cbbfa112e543fef05e887fcee
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-04-01 14:10:21 -0400

    create and read to database

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,4 +1,5 @@
 from flask import (Blueprint, render_template, request, redirect)
+from . import models
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
@@ -9,6 +10,15 @@ def new_facts():
 @bp.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['name']
+        fact = request.form['fact']
+        
+        new_fact = models.Fact(submitter=submitter, fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit() 
         return redirect('/facts')
-    return render_template('facts/index.html')
+    results = models.Fact.query.all()
+    for result in results:
+        print(result)
+    return render_template('facts/index.html', facts=results)
+
